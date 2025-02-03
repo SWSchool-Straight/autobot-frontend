bd39cd9f8eca7cc27efad1b4f54b445e9d673c9c
Yeonsu Lee <bestdustn@sookmyung.ac.kr>
2025-02-04 00:39:46 +0900

    chore: Update ESLint configuration to TypeScript and add TypeScript dependencies
    - ESLint 설정을 타입스크립트로 업데이트

---
+++ .eslintrc.ts
@@ -0,0 +1,48 @@
+import { ESLint } from 'eslint';
+
+const config: ESLint.ConfigData = {
+  root: true,
+  env: {
+    browser: true
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.ts'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    'react-refresh',
+    '@typescript-eslint'
+  ],
+  settings: {
+    react: {
+      version: '18.3'
+    }
+  },
+  rules: {
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true }
+    ],
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn'
+  }
+};
+
+export default config; 
